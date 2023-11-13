a4e4465cef9498d4dfbd4d587bfb88be47c775bb
voHAHAHA <sv.vov4ik@gmail.com>
2023-11-13 23:02:55 +0600

    First commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 fun main(args: Array<String>) {
+    // Лаб. 1
     // Задача 1.
     var numFirst : Int = 15
     var numSecond : Int = 23
@@ -40,6 +41,71 @@ fun main(args: Array<String>) {
     deck.sortDeck()
     println("\nОтсортированная колода: ")
     deck.printDeck()
+
+    // Лаб. 2
+    // Задача 1
+    println("Addition: ${sum(12, 53)}")
+    println("Addition: ${sum(5.6, 9.2)}") // Задача 2
+    println("Multiplication: ${multiply(5, 3, 4)}")
+    println("Multiplication: ${multiply(3.2, 4.5, 2.9)}") // Задача 2
+    greet("Vova")
+
+    // Задача 3
+    var cal = Calculator()
+    println(cal.add(12, 24))
+    println(cal.multiply(2, 5, 9))
+
+    // Задача 4
+    val intSumResult = CalculatorObj.sum(2, 3)
+    val doubleSumResult = CalculatorObj.sum(2.5, 3.5)
+    val intMultiplyResult = CalculatorObj.multiply(4, 6)
+    val doubleMultiplyResult = CalculatorObj.multiply(4.5, 2.5)
+
+    println("Sum of ints: $intSumResult")
+    println("Sum of doubles: $doubleSumResult")
+    println("Sum of ints: $intSumResult")
+    println("Sum of doubles: $doubleSumResult")
+}
+
+fun sum (num1 : Int, num2 : Int) : Int {
+    return num1 + num2
+}
+fun sum (num1 : Double, num2 : Double) : Double {
+    return num1 + num2
+}
+fun multiply (num1 : Int, num2 : Int, num3 : Int) : Int {
+    return num1 * num2 * num3
+}
+fun multiply (num1 : Double, num2 : Double, num3 : Double) : Double{
+    return num1 * num2 * num3
+}
+fun greet (name : String) {
+    println("Hello, $name")
+}
+
+class Calculator() {
+    fun add(num1 : Int, num2 : Int) : Int {
+        return num1 + num2
+    }
+    fun multiply (num1 : Int, num2 : Int, num3 : Int) : Int {
+        return num1 * num2 * num3
+    }
+}
+
+object CalculatorObj {
+    fun sum(a: Int, b: Int): Int {
+        return a + b
+    }
+    fun sum(a: Double, b: Double): Double {
+        return a + b
+    }
+    fun multiply(a: Int, b: Int): Int {
+        return a * b
+    }
+
+    fun multiply(a: Double, b: Double): Double {
+        return a * b
+    }
 }
 
 data class Card(val rank: String, val suit: String)
